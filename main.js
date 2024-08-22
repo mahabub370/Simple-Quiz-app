01b5d03f2ff30de9d6ec17cb81b1fbbe20e05fd1
mahabub370 <mahabub370@yahoo.com>
2024-08-23 01:25:38 +0600

    html and css is done now stat js

---
+++ main.js
@@ -0,0 +1,97 @@
+
+
+
+
+
+const questions = [
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    },
+    {
+        question : "Witch is the largest animal in the world?",
+        answers: [
+            {Text: "Shark", correct: false},
+            {Text: "Blue Whale", correct: true},
+            {Text: "Elephant", correct: false},
+            {Text: "Giraffe", correct: false}
+        ]
+    }
+];
